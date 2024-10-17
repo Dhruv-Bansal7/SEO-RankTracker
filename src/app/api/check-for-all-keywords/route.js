fd8857c05538c3cb481922d00ba6c62818be35bc
dhruv-bansal7 <dhruvbansal7727@gmail.com>
2024-10-17 18:45:24 +0530

    some updates in cron job

--- src/app/api/check-for-all-keywords/route.js
+++ src/app/api/check-for-all-keywords/route.js
@@ -4,30 +4,33 @@ import { Result } from "@/models/Result";
 import mongoose from "mongoose";
 import cron from "node-cron"
 
+
+async function connectToDB() {
+    if(mongoose.connection.readyState === 0){
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected To MongoDB");
+    }
+}
+
 async function checkAndSaveRanks() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await connectToDB();
         const keywordsDocs = await Keyword.find();
-        const googleSearchPromises = [];
-        const savePromises = []; 
-
-        for(const keywordDoc of keywordsDocs) {
-
-            const googleSearchPromise = getKeywordRank(keywordDoc.domain,keywordDoc.keyword);
-            googleSearchPromises.push(googleSearchPromise);
-            googleSearchPromise.then(rank => {
-                console.log({rank});
-                const savePromise = Result.create({
-                    domain: keywordDoc.domain,
-                    keyword: keywordDoc.keyword,
-                    position : rank,
-                });
-                savePromises.push(savePromise);
+        const googleSearchPromises = keywordsDocs.map(async (keywordsDoc) => {
+            const rank = await getKeywordRank(keywordsDoc.domain , keywordsDoc.keyword);
+            console.log({rank})
+            return Result.create({
+                domain : keywordsDoc.domain,
+                keyword : keywordsDoc.keyword,
+                position : rank,
             });
-        }
+        });
 
-        await Promise.allSettled([...googleSearchPromises, ...savePromises]);
-        console.log('Rank updates completed successfully.');
+        await Promise.allSettled(googleSearchPromises);
+        console.log("Rank updates completed Succesfully");
     } catch(err) {
         console.error('Error while updating ranks:', err);
     }
@@ -35,13 +38,12 @@ async function checkAndSaveRanks() {
 
 cron.schedule('0 0 * * *' , () => {
     console.log('Running scheduled keyword rank update at 12:00 AM');
-    checkAndSaveRanks();    
+    checkAndSaveRanks();
 })
 
 
 export async function GET() {
-    checkAndSaveRanks();
-
-    return Response.json({success : true}); 
+    await checkAndSaveRanks();
+    return new Response(JSON.stringify({succcess : true}) , {status : 200});
 }
 
